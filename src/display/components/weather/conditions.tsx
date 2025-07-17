a5a0aecb6153a16d8131f5fa83908c84204375d4
andrew867 <me@andrewgreen.ca>
2025-07-17 04:23:23 -0400

    fix: data handling

--- src/display/components/weather/conditions.tsx
+++ src/display/components/weather/conditions.tsx
@@ -12,9 +12,9 @@ type ConditionsProp = {
 };
 
 export function Conditions(props: ConditionsProp) {
-  const { city, conditions, stationTime, showPressure = false, airQuality } =  props ?? {};
+  const { city = "", conditions, stationTime, showPressure = false, airQuality } =  props ?? {};
   const { observedDateTime } = stationTime ?? {};
-  
+
   const {
     temperature = {},
     wind = {},
